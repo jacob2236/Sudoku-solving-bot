72ced26254cc0a40766d9e05d2e9a940c33b07b6
aww-shoot <alicewritesthings@proton.me>
2026-01-27 17:08:29 -0500

    fresh data collection system

--- test_sudoku.cpp
+++ test_sudoku.cpp
@@ -8,75 +8,78 @@
 #include "fitness_functions.hpp"
 #include "class_represented_alldifferent.hpp"
 #include "crossover_functions.hpp"
+#include <fstream>
 using namespace std;
 
 
 
 int main() {
     srand(1); // set the seed
-    SudokuBoard board = getRandomBoard();
-    // give it to a representation
-    RepresentedAllDifferent repBoard(board);
-    // make the abstract board
-    AbstractBoard abstractBoard(countConflicts, fillPossibilityOrRemove, dummy, repBoard);
-    //test
-    board.print();
-    cout << "solving...\n";
-    AbstractBoard solvedBoard = randomBruteForce(abstractBoard, 1000);
-    solvedBoard.print();
-
-
-
-    board = getRandom17Board();
-    
-    // give it to a representation
-    repBoard = RepresentedAllDifferent(board);
-    // make the abstract board
-    abstractBoard = AbstractBoard(countConflicts, fillPossibilityOrRemove, dummy, repBoard);
-    //test
-    board.print();
-    cout << "solving...\n";
-    solvedBoard = randomBruteForce(abstractBoard, 1000);
-    solvedBoard.print();
-
-
-    board = getRandomHardBoard();
-    
-    // give it to a representation
-    repBoard = RepresentedAllDifferent(board);
-    // make the abstract board
-    abstractBoard = AbstractBoard(countConflicts, fillPossibilityOrRemove, dummy, repBoard);
-    //test
-    board.print();
-    cout << "solving...\n";
-    solvedBoard = randomBruteForce(abstractBoard, 1000);
-    solvedBoard.print();
-
-    cout << endl; // flush buffer
-    return 0;
 
     //Data collection
+
+    //DOUBLE CHECK AND CHANGE BEFORE EACH RUN
+    ofstream fileWriter("solver_data/testrunoutput.txt");
+    fileWriter << "Board, Trial, Time, Fitness, solved\n";
+    fileWriter << "\t, average time, average fitness, average solved";
+
     double average_time = 0; 
     double average_solve = 0;
+    double average_fitness = 0;
+    double total_time = 0;
+    double total_fitness = 0;
     int boards_solved = 0;
-    int num_of_boards = 20;
+    int boards_per_study = 20;
     int num_trials_per_board = 5;
     clock_t start, end;
+    for (int board_count = 1; board_count < boards_per_study; board_count++ ) {
+        SudokuBoard initialBoard = getRandomBoard();
+        cout << "New board for 5 trials! This is board number" << board_count << "\n";
+        initialBoard.print();
+        for (int trial=1;trial<=num_trials_per_board;trial++) {
+            clock_t c_start = clock();
+            start = clock();
 
-    for (int trial=1;trial<=num_trials_per_board;trial++) {
-        clock_t c_start = clock();
-        start = clock();
+            // DO tests
+            // give it to a representation
+            RepresentedAllDifferent repBoard(initialBoard);
+            // make the abstract board
+            AbstractBoard abstractBoard(countConflicts, fillPossibilityOrRemove, dummy, repBoard);
+            cout << "solving...\n";
+            AbstractBoard solvedBoard = geneticAlgorithm(abstractBoard, 1000, .6, 0.8);
+            solvedBoard.print();
 
-        // DO tests
-        AbstractBoard test;
+            end = clock();
+            double time_taken = double(end - start);
+            int fitness = solvedBoard.getFitness();
+            cout << "Attempt #" << trial << "trial\n";
+            cout << "Best fitness: " << fitness << "trial\n";
+            cout << "Time(CPU ticks): " << time_taken << "trial\n";
+            total_time = total_time + time_taken;
+            total_fitness += fitness;
+            fileWriter << board_count << ", " << trial << ", " << time_taken << ", " << fitness << ", ";
+            if (fitness == 0) {
+                boards_solved = boards_solved + 1;
+                fileWriter << "1\n";
+            } else {
+                fileWriter << "0\n";
+            }
+            
+            
+            cout << "--------------------------------------trial\n";
+        }
+        average_time = total_time / 100.0;
+        average_fitness = total_fitness / 100.0;
+        average_solve = boards_solved / 100.0;
+        fileWriter << "\t" << average_time << ", " << average_fitness << ", " << average_solve << "\n";
+        cout << "average time: " << average_time;
+        cout << "\naverage fitness: " << average_fitness;
+        cout << "\naverage solved: " << average_solve;
 
-        end = clock();
-        double time_taken = double(end - start);
-        cout << "Attempt #" << trial << "\trial";
-        cout << "Best fitness: " << test.getFitness() << "\trial";
-        cout << "Time(CPU ticks): " << time_taken << "\trial";
-        cout << "--------------------------------------\trial";
     }
+    
+
+    
 
 
 }
